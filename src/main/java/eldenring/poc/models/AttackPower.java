fba9d27608aa720f83d39f12999c6bed9bf1817c
Tiago Siqueira Dionizio <tiagosdionizio@gmail.com>
2025-10-22 19:26:23 -0300

    Add AttackPower model class
    Introduces the AttackPower class with name and amount fields, including getters and setters. This model will be used to represent attack power attributes in the application.

---
+++ src/main/java/eldenring/poc/models/AttackPower.java
@@ -0,0 +1,26 @@
+package eldenring.poc.models;
+
+public class AttackPower {
+    private String name;
+    private int amount;
+
+    public AttackPower() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
+
