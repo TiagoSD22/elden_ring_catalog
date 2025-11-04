147b0dda592b508d08affdb239a885b6b9f44254
Tiago Siqueira Dionizio <tiagosdionizio@gmail.com>
2025-11-04 18:05:06 -0300

    Add SpiritBase model class
    Introduces the SpiritBase class extending BaseModel, with title and image fields, constructors, getters/setters, and overrides for display name, image URL, and toString. This provides a base model for spirit entities in the application.

---
+++ src/main/java/eldenring/poc/models/SpiritBase.java
@@ -0,0 +1,50 @@
+package eldenring.poc.models;
+
+public class SpiritBase extends BaseModel {
+    private static final long serialVersionUID = 1L;
+
+    private String title;
+    private String image;
+
+    public SpiritBase() {}
+
+    public SpiritBase(String title, String image) {
+        this.title = title;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return title;
+    }
+
+    @Override
+    public String getImageUrl() {
+        return image;
+    }
+
+    @Override
+    public String toString() {
+        return "SpiritBase{" +
+                "title='" + title + '\'' +
+                ", image='" + image + '\'' +
+                '}';
+    }
+}
+
