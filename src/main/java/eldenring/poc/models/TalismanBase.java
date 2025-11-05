454de961292d69d35097c5641d5386c16e1c5199
Tiago Siqueira Dionizio <tiagosdionizio@gmail.com>
2025-11-05 18:08:07 -0300

    Add TalismanBase model class
    Introduces the TalismanBase class extending BaseModel, with title and image fields, constructors, getters/setters, and overrides for display name, image URL, and toString. This provides a base model for talisman entities.

---
+++ src/main/java/eldenring/poc/models/TalismanBase.java
@@ -0,0 +1,50 @@
+package eldenring.poc.models;
+
+public class TalismanBase extends BaseModel {
+    private static final long serialVersionUID = 1L;
+
+    private String title;
+    private String image;
+
+    public TalismanBase() {}
+
+    public TalismanBase(String title, String image) {
+        this.title = title;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return title;
+    }
+
+    @Override
+    public String getImageUrl() {
+        return image;
+    }
+
+    @Override
+    public String toString() {
+        return "TalismanBase{" +
+                "title='" + title + '\'' +
+                ", image='" + image + '\'' +
+                '}';
+    }
+}
+
